fc2dff8876ea62bac07515b75ba9bf9e8672cc13
YangGu <gy3420@163.com>
2020-02-03 22:21:45 +0800

    2020/2/3 leetcode随机两题 目前进度64/1345

---
+++ 405_toHex.cpp
@@ -0,0 +1,116 @@
+﻿/*
+ * Author: YangGu 
+ *   Date: 2020/2/3
+*/
+
+#define CATCH_CONFIG_MAIN  
+
+#include <catch.hpp>
+#include <string>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+/*
+ * 数字转换为十六进制数
+ * 十六进制中所有字母(a-f)都必须是小写。
+ * 十六进制字符串中不能包含多余的前导零。
+ * 如果要转化的数为0，那么以单个字符'0'来表示；
+ * 对于其他情况，十六进制字符串中的第一个字符将不会是0字符。
+ * 给定的数确保在32位有符号整数范围内。
+ * 不能使用任何由库提供的将数字直接转换或格式化为十六进制的方法。
+*/
+const int HEX_MOD = 16;
+const int DEC_MOD = 10;
+string nonNegativetoHex(long long num);
+
+string toHex(int num)
+{
+    long long dw_num = num;
+    if (num < 0)
+    {
+        dw_num = 0x100000000 + num;
+    }
+
+    return nonNegativetoHex(dw_num);
+}
+
+string nonNegativetoHex(long long num)
+{
+    if (num == 0)
+    {
+        return "0";
+    }
+    string hex_str = "";
+    while (num != 0)
+    {
+        int mod = num % HEX_MOD;
+        if (mod >= DEC_MOD)
+        {
+            hex_str += ('a' + mod - DEC_MOD);
+        }
+        else
+        {
+            hex_str += ('0' + mod);
+        }
+        num = num / HEX_MOD;
+    }
+
+    reverse(hex_str.begin(), hex_str.end());
+
+    return hex_str;
+}
+
+SCENARIO("decimal to hex") 
+{
+    GIVEN("toHex") 
+    {
+        int input_decimal_num = 0;
+
+        WHEN("input 26") 
+        {
+            input_decimal_num = 26;
+            THEN("get 1a") 
+            {
+                REQUIRE(toHex(input_decimal_num) == "1a");
+            }
+        }
+
+        WHEN("input -1")
+        {
+            input_decimal_num = -1;
+            THEN("get ffffffff")
+            {
+                REQUIRE(toHex(input_decimal_num) == "ffffffff");
+            }
+        }
+
+        WHEN("input 0")
+        {
+            input_decimal_num = 0;
+            THEN("get 0")
+            {
+                REQUIRE(toHex(input_decimal_num) == "0");
+            }
+        }
+
+        WHEN("input 1")
+        {
+            input_decimal_num = 1;
+            THEN("get 1")
+            {
+                REQUIRE(toHex(input_decimal_num) == "1");
+            }
+        }
+
+        WHEN("input -270")
+        {
+            input_decimal_num = -270;
+            THEN("get fffffef2")
+            {
+                REQUIRE(toHex(input_decimal_num) == "fffffef2");
+            }
+        }
+    }
+}
+
---
+++ 997_findJudge.cpp
@@ -0,0 +1,91 @@
+﻿/*
+ * Author: YangGu 
+ *   Date: 2020/2/3
+*/
+
+#define CATCH_CONFIG_MAIN  
+
+#include <catch.hpp>
+#include <vector>
+
+
+using namespace std;
+/*
+在一个小镇里，按从 1 到 N 标记了 N 个人。传言称，这些人中有一个是小镇上的秘密法官。
+如果小镇的法官真的存在，那么：
+
+    小镇的法官不相信任何人。
+    每个人（除了小镇法官外）都信任小镇的法官。
+    只有一个人同时满足属性 1 和属性 2 。
+
+给定数组 trust，该数组由信任对 trust[i] = [a, b] 组成，表示标记为 a 的人信任标记为 b 的人。
+如果小镇存在秘密法官并且可以确定他的身份，请返回该法官的标记。否则，返回 -1。
+*/
+
+int findJudge(int N, vector<vector<int>>& trust)
+{
+    vector<int> trust_cnt(N+1, 0);
+
+    for (int i = 0; i < trust.size(); i++)
+    {
+        trust_cnt[trust[i][1]] ++;
+    }
+    int judge = -1;
+    for (int i = 1; i < trust_cnt.size(); i++)
+    {
+        if (trust_cnt[i] == N - 1)
+        {
+            judge = i;
+            continue;
+        }
+    }
+    for (int i = 0; i < trust.size(); i++)
+    {
+        if (judge == trust[i][0])
+        {
+            judge = -1;
+            continue;
+        }
+    }
+    return judge;
+}
+
+SCENARIO("findJudge in trust vector") 
+{
+    GIVEN("findJudge") 
+    {
+        WHEN("input trust = [[1,2]], N = 2") 
+        {
+            int N = 2;
+            vector<vector<int>> trust_vector = { {1,2} };
+
+            THEN("judge is 2") 
+            {
+                REQUIRE(findJudge(N, trust_vector) == 2);
+            }
+        }
+
+        WHEN("input N = 3, trust = [[1,3],[2,3]]")
+        {
+            int N = 3;
+            vector<vector<int>> trust_vector = { {1,3},{2,3} };
+
+            THEN("judge is 3")
+            {
+                REQUIRE(findJudge(N, trust_vector) == 3);
+            }
+        }
+
+        WHEN("input N = 4, trust = [[1,3],[1,4],[2,3],[2,4],[4,3]]")
+        {
+            int N = 4;
+            vector<vector<int>> trust_vector = { {1,3},{1,4},{2,3},{2,4},{4,3} };
+
+            THEN("judge is 3")
+            {
+                REQUIRE(findJudge(N, trust_vector) == 3);
+            }
+        }
+    }
+}
+
