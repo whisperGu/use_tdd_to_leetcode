acd6297928fe2c8f4b5e4148ce0fff779586ec53
YangGu <gy3420@163.com>
2020-02-06 20:57:04 +0800

    2020/2/4 leetcode随机两题 目前进度70/1345

---
+++ 200_numIslands.cpp
@@ -0,0 +1,93 @@
+﻿/*
+ * Author: YangGu 
+ *   Date: 2020/2/6
+*/
+
+#define CATCH_CONFIG_MAIN  
+
+#include <catch.hpp>
+#include <vector>
+
+using namespace std;
+
+ /**
+    给定一个由 '1'（陆地）和 '0'（水）组成的的二维网格，计算岛屿的数量。一个岛被水包围，
+    并且它是通过水平方向或垂直方向上相邻的陆地连接而成的。你可以假设网格的四个边均被水包围。
+
+    示例 1:
+
+    输入:
+    11110
+    11010
+    11000
+    00000
+
+    输出: 1
+  */
+void dfs(vector<vector<char>>& grid, vector<vector<bool>>& visited, int i, int j)
+void dfs(vector<vector<char>>& grid, vector<vector<bool>>& visited, int i, int j)
+{
+	int nr = grid.size();
+    int nc = grid[0].size();
+	
+    if (i >= nr || j >= nc|| i < 0 || j < 0)
+    {
+        return;
+    }
+    if (grid[i][j] == '0' || visited[i][j] == true)
+    {
+        return;
+    }
+
+    visited[i][j] = true;
+
+    dfs(grid, visited, i+1, j);
+    dfs(grid, visited, i, j+1);
+    dfs(grid, visited, i-1, j);
+    dfs(grid, visited, i, j-1);
+}
+
+int numIslands(vector<vector<char>>& grid) 
+{
+    /* dfs 找矩阵连通区域个数 */
+    int n_rows = grid.size();
+    if (n_rows == 0)
+    {
+        return 0;
+    }
+    int n_cols = grid[0].size();
+
+
+    vector<vector<bool>> visited(n_rows, vector<bool>(n_cols, false));
+    int res = 0;
+    for (int i = 0; i < n_rows; i++)
+    {
+        for (int j = 0; j < n_cols; j++)
+        {
+            if (grid[i][j] == '1' && visited[i][j] == false)
+            {
+                res++;
+                dfs(grid, visited, i , j);              
+            }
+        }
+    }
+    return res;
+}
+
+SCENARIO("numIslands ") 
+{
+    GIVEN("numIslands") 
+    {
+        WHEN("grid is \n11110\n11010\n11000\n00000") 
+        {
+            vector<vector<char>> grid = { {1,1,1,1,0} ,{1,1,0,1,0} ,{1,1,0,0,0} ,{0,0,0,0,0} };
+            THEN("Island number is 1 ") 
+            {
+                REQUIRE(numIslands(grid) == 1);
+            }
+        }
+    }
+}
+
+
+
---
+++ 669_trimBST.cpp
@@ -0,0 +1,101 @@
+﻿/*
+ * Author: YangGu 
+ *   Date: 2020/2/6
+*/
+
+#define CATCH_CONFIG_MAIN  
+
+#include <catch.hpp>
+#include <vector>
+
+using namespace std;
+
+ /**
+ 669
+给定一个二叉搜索树，同时给定最小边界L 和最大边界 R。通过修剪二叉搜索树，
+使得所有节点的值在[L, R]中 (R>=L) 。你可能需要改变树的根节点，
+所以结果应当返回修剪好的二叉搜索树的新的根节点。
+
+示例 1:
+
+输入:
+    1
+   / \
+  0   2
+
+  L = 1
+  R = 2
+
+输出:
+    1
+      \
+       2
+
+
+示例 2:
+
+输入:
+    3
+   / \
+  0   4
+   \
+    2
+   /
+  1
+
+  L = 1
+  R = 3
+
+输出:
+      3
+     /
+   2
+  /
+ 1
+  */
+
+struct TreeNode 
+{
+     int val;
+     TreeNode *left;
+     TreeNode *right;
+     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ };
+
+class Solution {
+public:
+    TreeNode* trimBST(TreeNode* root, int L, int R) 
+    {
+        /*
+        1.当前节点不在区间，比最小值小，剪掉左边，用右子树替代，再遍历新节点，更新root
+        2.当前节点不在区间，比最大致大，剪掉右边，用左子树替代，再遍历新节点，更新root
+        3.节点在区间，递归遍历左边和右边 ，return root
+        */
+        if (root == NULL)
+        {
+            return NULL;
+        }
+        if (root->val < L)
+        {
+            root->left = NULL;
+            root = trimBST(root->right, L, R);
+            return root;
+        }
+        else if (root->val > R)
+        {
+            root->right = NULL;
+            root = trimBST(root->left, L, R);
+            return root;
+        }
+        else
+        {
+            root->left = trimBST(root->left, L, R);
+            root->right = trimBST(root->right, L, R);
+            return root;
+
+        }
+
+        return root;
+    }
+};
+
