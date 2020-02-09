93bbb79c736189961f9baa0bc454d8dbe9cd1725
YangGu <gy3420@163.com>
2020-02-09 23:15:28 +0800

    2020/2/4 leetcode随机两题 目前进度76/1345

---
+++ 700_searchBST.cpp
@@ -0,0 +1,22 @@
+/*
+给定二叉搜索树（BST）的根节点和一个值。 你需要在BST中找到节点值等于给定值的节点。 返回以该节点为根的子树。 如果节点不存在，则返回 NULL。
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        if(!root) return nullptr;
+        if(root->val == val) return root;
+        else if(root->val < val) return searchBST(root->right, val);
+        else return searchBST(root->left, val);
+    }
+};
